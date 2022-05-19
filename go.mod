d89626bd39c9be39a62bcdf300e3dd20593e9108
kyo <mathis.charretier.42@gmail.com>
2022-05-19 16:49:43 +0000

    update

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/gitpod/mycli
 go 1.16
 
 require (
+	github.com/gofiber/fiber v1.14.6
+	github.com/jinzhu/gorm v1.9.16
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.1.3
 	github.com/spf13/viper v1.7.1
--- main.go
+++ main.go
@@ -15,8 +15,25 @@ limitations under the License.
 */
 package main
 
-import "github.com/gitpod/mycli/cmd"
+import (
+	"github.com/matcharr/go-fiber-crm-basic/database"
+	"github.com/gofiber/fiber"
+)
+
+func setupRoutes(app *fiber.App) {
+	app.Get(GetLeads)
+	app.Get(GetLead)
+	app.Post(NewLead)
+	app.Delete(DeleteLead)
+}
+
+func initDatabase() {
+
+}
 
 func main() {
-	cmd.Execute()
+	app := fiber.New()
+	setupRoutes(app)
+	app.Listen(3000)
+
 }
